89e6448978de545735f6e3f99729d326eb794a7d
Adrian Smith <adrianrsmith93@gmail.com>
2015-05-30 00:04:09 -0700

    Fixed Gaben script, and removed the Netbeans files from the repo. Signed-off-by:Adrian Smith <adrianrsmith93@gmail.com>

--- public_html/forum/header.php
+++ public_html/forum/header.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Rancho&effect=fire-animation">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<!--<script src="../js/site.js"></script>-->
-<script src="../js/debug.js"></script>
+<script src="../js/site.js"></script>
+<!--<script src="../js/debug.js"></script>-->
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
